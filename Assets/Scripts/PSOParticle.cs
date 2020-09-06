c4941b4d3666c4d9d1fd31e12e4cfd7a48145c8b
Diogo Andrade <diogo.g.andrade@gmail.com>
2020-09-06 15:21:38 +0100

    First experiments

---
+++ Assets/Scripts/PSOParticle.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PSOParticle : MonoBehaviour
+{
+    struct UpdateItem
+    {
+        public float    time;
+        public Vector3  position;
+    };
+
+    public int      particleId;
+    public Color    color = Color.white;
+    public float    scale = 1.0f;
+    public float    totalTime;
+
+    List<UpdateItem> positions;
+    int              index;
+    float            elapsedTime = 0.0f;
+    
+    void Start()
+    {
+        index = 0;
+
+        TrailRenderer tr = GetComponent<TrailRenderer>();
+        if (tr)
+        {
+            tr.time = totalTime * 0.0005f;
+            tr.startColor = color;
+            tr.endColor = new Color(color.r, color.g, color.a, 0.0f);
+        }
+    }
+
+    void Update()
+    {
+        elapsedTime += Time.deltaTime;
+        
+        if ((index + 1) < positions.Count)
+        {
+            if (elapsedTime >= positions[index + 1].time)
+            {
+                index = index + 1;
+            }
+
+            if ((index + 1) < positions.Count)
+            {
+                Vector3 p1 = positions[index].position;
+                Vector3 p2 = positions[index + 1].position;
+
+                float t = (elapsedTime - positions[index].time) / (positions[index + 1].time - positions[index].time);
+                transform.position = Vector3.Lerp(p1, p2, t);
+            }
+        }
+    }
+
+    public void AddUpdateAction(float time, float x, float y, float z)
+    {
+        if (positions == null) positions = new List<UpdateItem>();
+
+        positions.Add(new UpdateItem { time = time, position = new Vector3(x, y, z) });
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawSphere(transform.position, scale * 0.125f);
+    }
+}
---
+++ Assets/Scripts/PSORender.cs
@@ -0,0 +1,96 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PSORender : MonoBehaviour
+{
+    public TextAsset    runData;
+    public float        timePerIteration = 1.0f;
+    public PSOParticle  particlePrefab;
+    public Gradient     colorParticles;
+
+    [Header("References")]
+    public Camera       mainCamera;
+
+    [Header("Runtime")]
+    public Rect         boundary;
+    public float        totalTime;
+
+    List<PSOParticle> particles;
+    
+    void Start()
+    {
+        particles = new List<PSOParticle>();
+
+        var splitFile = new string[] { "\r\n", "\r", "\n" };
+        var splitLine = new string[] { ";" };
+        var lines = runData.text.Split(splitFile, System.StringSplitOptions.RemoveEmptyEntries);
+
+        float x1 = float.MaxValue;
+        float z1 = float.MaxValue;
+        float x2 = -float.MaxValue;
+        float z2 = -float.MaxValue;
+
+        totalTime = 0.0f;
+
+        for (int idx = 1; idx < lines.Length; idx++)
+        {
+            var line = lines[idx];
+            var tokens = line.Split(splitLine, System.StringSplitOptions.None);
+            int action = int.Parse(tokens[0]);
+            int iteration = int.Parse(tokens[1]);
+            int particleId = int.Parse(tokens[2]);
+            float x = float.Parse(tokens[3]);
+            float z = float.Parse(tokens[4]);
+            float y = float.Parse(tokens[5]);
+
+            x1 = Mathf.Min(x1, x);
+            x2 = Mathf.Max(x2, x);
+            z1 = Mathf.Min(z1, z);
+            z2 = Mathf.Max(z2, z);
+
+            totalTime = Mathf.Max(totalTime, iteration * timePerIteration);
+
+            if (particles.Count <= particleId)
+            {
+                for (int i = particles.Count; i <= particleId; i++) particles.Add(null);
+            }
+
+            if (particles[particleId] == null)
+            {
+                // Create new particle
+                particles[particleId] = Instantiate(particlePrefab);
+                particles[particleId].name = "Particle " + particleId;
+                particles[particleId].particleId = particleId;
+                particles[particleId].transform.position = new Vector3(x, 0, z);
+            }
+
+            if (action == 0)
+            {
+                particles[particleId].AddUpdateAction(iteration * timePerIteration, x, y, z);
+            }
+            else
+            {
+                Debug.Assert(false, "Unknown action!");
+            }
+        }
+
+        boundary.Set(x1, z1, x2 - x1, z2 - z1);
+
+        float maxExtent = Mathf.Max(boundary.height, boundary.width);
+        mainCamera.orthographicSize = (maxExtent * 0.5f) * 1.05f;
+
+        float scale = maxExtent / 10.0f;
+        foreach (var particle in particles)
+        {
+            particle.scale = scale;
+            particle.color = colorParticles.Evaluate(Random.Range(0.0f, 1.0f));
+            particle.totalTime = totalTime;
+        }
+    }
+
+    void Update()
+    {
+        
+    }
+}
