6234f2b531373499da1ed30b5cf8dca59a40956c
Diogo Andrade <diogo.g.andrade@gmail.com>
2020-09-08 21:06:57 +0100

    Forgot files

---
+++ Assets/Scripts/PSOFunction.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PSOFunction : MonoBehaviour
+{
+    public Vector2Int nPoints;
+    public Rect       extents;
+    public float      density;
+    public float      minY, maxY;
+
+    protected float[,] functionValues;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Parse(TextAsset data)
+    {
+        var splitFile = new string[] { "\r\n", "\r", "\n" };
+        var splitLine = new string[] { " " };
+        var lines = data.text.Split(splitFile, System.StringSplitOptions.RemoveEmptyEntries);
+
+        nPoints = new Vector2Int(int.Parse(lines[0]), int.Parse(lines[1]));
+        float x1 = float.Parse(lines[2]);
+        float y1 = float.Parse(lines[3]);
+        float x2 = float.Parse(lines[4]);
+        float y2 = float.Parse(lines[5]);
+        extents = new Rect(x1, y1, x2 - x1, y2 - y1);
+        density = float.Parse(lines[6]);
+
+        functionValues = new float[nPoints.x,nPoints.y];
+
+        minY = float.MaxValue;
+        maxY = -float.MaxValue;
+
+        for (int y = 7; y < lines.Length; y++)
+        {
+            var vals = lines[y].Split(splitLine, System.StringSplitOptions.None);
+
+            for (int x = 0; x < nPoints.x; x++)
+            {
+                float v = float.Parse(vals[x]);
+                functionValues[x, y - 7] = v;
+                minY = Mathf.Min(v, minY);
+                maxY = Mathf.Max(v, maxY);
+            }
+        }
+
+        OnDataProcessed();
+    }
+
+    virtual protected void OnDataProcessed()
+    {
+
+    }
+
+    protected void NormalizeData()
+    {
+        for (int y = 0; y < nPoints.y; y++)
+        {
+            for (int x = 0; x < nPoints.x; x++)
+            {
+                functionValues[x, y] = (functionValues[x, y] - minY) / (maxY - minY);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/PSOFunctionTerrain.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PSOFunctionTerrain : PSOFunction
+{
+    override protected void OnDataProcessed()
+    {
+        Terrain terrain = GetComponent<Terrain>();
+
+        NormalizeData();
+        terrain.terrainData.size = new Vector3(extents.width, (maxY - minY), extents.height);
+        terrain.terrainData.heightmapResolution = nPoints.x;
+        terrain.terrainData.SetHeights(0, 0, functionValues);
+
+        transform.position = new Vector3(extents.x, minY, extents.y);
+    }
+
+}
